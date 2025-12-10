867cb7e214309f6993c2f905a62349f78ed98f3e
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-10 11:44:18 +0530

    user registration done

---
+++ Backend/db/db.js
@@ -0,0 +1,12 @@
+const e = require('express');
+const mongoose = require('mongoose');
+
+
+function connectToDB() {
+    mongoose.connect(process.env.DB_CONNECT
+    ).then( () => {
+            console.log('Connected to DB');
+        }).catch(err => console.log(err));
+}
+
+module.exports = connectToDB;
