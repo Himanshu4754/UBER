d82f95b6e5b16093ecdec08643c64d613b4be06c
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-13 09:48:33 +0530

    user authentication done

--- Backend/README.md
+++ Backend/README.md
@@ -56,3 +56,47 @@ The request body should be in JSON format and include the following fields:
     - `lastname` (string): User's last name.
   - `email` (string): User's email address.
 - `token` (String): JWT Token
+
+---
+
+## `/users/profile` Endpoint
+
+### Description
+
+Retrieves the profile information of the currently authenticated user. This route is protected and requires a valid JWT.
+
+### HTTP Method
+
+`GET`
+
+### Authentication
+
+Requires a valid JWT token in the `Authorization` header as a Bearer token or in the `token` cookie.
+
+### Example Response
+
+- `user` (object):
+  - `fullname` (object):
+    - `firstname` (string): User's first name.
+    - `lastname` (string): User's last name.
+  - `email` (string): User's email address.
+
+---
+
+## `/users/logout` Endpoint
+
+### Description
+
+Logs out the current user by invalidating the provided JWT token and adding it to the blacklist.
+
+### HTTP Method
+
+`GET`
+
+### Authentication
+
+Requires a valid JWT token in the `Authorization` header as a Bearer token or in the `token` cookie.
+
+### Example Response
+
+- `message` (string): "Logged out successfully"
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.services');
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req,res,next) => {
     
@@ -21,7 +22,7 @@ module.exports.registerUser = async (req,res,next) => {
     });
 
     const token = user.generateAuthToken();
-
+    res.cookie('token', token);
     res.status(201).json({token,user});     
 }
 
@@ -40,5 +41,20 @@ module.exports.loginUser = async (req,res,next) => {
         return res.status(401).json({error: 'Invalid email or password'});
     }
     const token = user.generateAuthToken();
+    res.cookie('token', token);
     res.status(200).json({token,user});
+}
+
+module.exports.getUserProfile = async (req,res,next) => {
+    const userId = req.user._id;
+    const user = await userModel.findById(userId).select('-password');
+    res.status(200).json({user});
+}
+
+module.exports.logoutUser = async (req,res,next) => {
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    
+    await blacklistTokenModel.create({token});
+    res.clearCookie('token');
+    res.status(200).json({message: 'Logged out successfully'});
 }
