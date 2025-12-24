b4c83aeb31d39e525d462aabc3358ebe70d22755
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-24 09:53:54 +0530

    Prepare for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,21 @@
 const http = require('http');
 const app = require('./app');
+const path = require('path');
+const express = require('express');
 const { initializeSocket } = require('./socket');
-const connectToDB = require('./db/db')
+const connectToDB = require('./db/db');
 
 const port = process.env.PORT || 3000;
 
 connectToDB();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    });
+}
 
 const server = http.createServer(app);
 
