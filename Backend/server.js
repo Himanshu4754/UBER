8ff04574892bc83aa5e889e3681eaf1a3deae973
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-24 10:36:59 +0530

    Simplify server.js to fix deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,11 @@
 const http = require('http');
 const app = require('./app');
-const path = require('path');
 const { initializeSocket } = require('./socket');
 const connectToDB = require('./db/db');
 
 const port = process.env.PORT || 3000;
 
 connectToDB();
-
-if (process.env.NODE_ENV === 'production') {
-    const express = require('express');
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    
-    app.use((req, res, next) => {
-        if (!req.path.startsWith('/api') && 
-            !req.path.startsWith('/users') && 
-            !req.path.startsWith('/captains') && 
-            !req.path.startsWith('/maps') && 
-            !req.path.startsWith('/rides')) {
-            res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-        } else {
-            next();
-        }
-    });
-}
 
 const server = http.createServer(app);
 
