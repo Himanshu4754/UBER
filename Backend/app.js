79771214908eef0343375a5c43cb9473558f3586
Himanshu4754 <himanshugurjar1306@gmail.com>
2025-12-24 10:58:20 +0530

    Fix catch-all route for frontend serving

--- Backend/app.js
+++ Backend/app.js
@@ -19,18 +19,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
 app.use('/rides', rideRoutes);
 
+
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
